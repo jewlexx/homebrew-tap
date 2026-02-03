e1639043fd3d922230c87ca7071db43f351fe9f0
Juliette <61040004+jewlexx@users.noreply.github.com>
2026-02-03 07:16:32 +0000

    rmx: add 0.2.2 bottle.

--- Formula/rmx.rb
+++ Formula/rmx.rb
@@ -5,6 +5,12 @@ class Rmx < Formula
   sha256 "25f2a86b5af770059ab7f1af74b0a693e380d4f7f08cd2729864593728eb4c0b"
   license "GPL-3.0-or-later"
 
+  bottle do
+    root_url "https://github.com/jewlexx/homebrew-tap/releases/download/rmx-0.2.2"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:  "a62be9b77e9200bd3e85e996407dd20496ba97463d37a37b9f813d45e7490fd4"
+    sha256 cellar: :any_skip_relocation, x86_64_linux: "65aff3007930102cbfb242b7bb836414a755127e73644361fc88dba88cd983f6"
+  end
+
   depends_on "rust" => :build
 
   def install
